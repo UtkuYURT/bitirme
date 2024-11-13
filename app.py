d28fa337beb65d9836adc9f8da1a722ee0311988
UtkuYURT <213405075@ogrenci.ibu.edu.tr>
2024-11-13 19:46:08 +0300

    mysql bağlantısı yapıldı
    kullanıcı kaydı ve girişi yapıldı

--- app.py
+++ app.py
@@ -1,18 +1,56 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+from db import init_db, sign_up, sign_in
 
 app = Flask(__name__)
+
+app.secret_key = 'gizli_anahtar'
+
+# ? DB Settings 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'bitirme'
+
+init_db(app)
 
 @app.route('/')
 def home():
     return render_template('index.html')
-    # render_template('index.html', title="Deneme", message="Python'dan Deneme")                      
+
+@app.route('/', methods=['POST'])
+def sign_in_route():
+    if request.method == 'POST':
+        email = request.form['e-mail']
+        password = request.form['sifre']
+        user = sign_in(email, password)
+        if user:
+            flash("Giriş Başarılı", "success") 
+            return redirect(url_for('main_page')) 
+        else:
+            flash("Giriş Başarısız", "danger")
+            return redirect(url_for('home')) 
 
 @app.route('/register')
 def register():
     return render_template('register.html')
 
+@app.route('/register', methods=['POST'])
+def sign_up_route():
+    if request.method == 'POST':
+        email = request.form['e-mail']
+        password1 = request.form['sifre']
+        password2 = request.form['sifre2']
+        if password1 == password2:
+            sign_up(email, password1)
+            flash("Kayıt Başarılı", "success")
+            return redirect(url_for('home')) 
+        else:
+            flash("Kayıt Başarısız", "danger")
+            return redirect(url_for('register'))  
+
+@app.route('/main_page')
+def main_page():
+    return render_template('main_page.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-# ! SA ! #
---
+++ db.py
@@ -0,0 +1,20 @@
+from flask_mysqldb import MySQL
+
+mysql = None
+
+def init_db(app):
+    global mysql
+    mysql = MySQL(app)
+
+def sign_in(email, password):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM users WHERE email = %s AND password = %s", (email, password))
+    user = cur.fetchone()
+    cur.close()
+    return user
+
+def sign_up(email, password):
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO users (email, password) VALUES (%s, %s)", (email, password))
+    mysql.connection.commit()
+    cur.close()
