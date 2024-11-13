87f294a747ef05093ae1efe748629ca6a5baca14
UtkuYURT <213405075@ogrenci.ibu.edu.tr>
2024-11-13 21:56:30 +0300

    mysql her pc için

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ app = Flask(__name__)
 app.secret_key = 'gizli_anahtar'
 
 # ? DB Settings 
-app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', 'localhost')
-app.config['MYSQL_USER'] = os.getenv('MYSQL_USER', 'root')
-app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD', 'root')
-app.config['MYSQL_DB'] = os.getenv('MYSQL_DB', 'bitirme')
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'bitirme'
 
 init_db(app)
 
